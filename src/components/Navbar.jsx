f90d0ac4e22b7beeca0d84d624058d338df67eb6
MaxPrain12 <alder.velasquezobando@gmail.com>
2022-02-07 12:10:57 +0100

    2 files modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
-import {
-  Nav,
-  NavDropdown,
-  Navbar,
-  Offcanvas,
-  Container,
-  Form,
-  FormControl,
-  Button,
-} from 'react-bootstrap';
+import { Nav, Navbar, Offcanvas, Container } from 'react-bootstrap';
 
 class Navbar extends React.Component {
   constructor(props) {
@@ -35,26 +26,7 @@ class Navbar extends React.Component {
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link href="#action1">Home</Nav.Link>
                 <Nav.Link href="#action2">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
               </Nav>
-              <Form className="d-flex">
-                <FormControl
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
